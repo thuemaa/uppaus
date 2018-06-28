b23d7d6c81983a64dfceeb385edfa77f89af467f
thuemaa <35839876+thuemaa@users.noreply.github.com>
2018-06-28 20:55:23 +0300

    links + css
    Added next and prev page for images.
    fiddled with the css

--- upploud/views.py
+++ upploud/views.py
@@ -2,29 +2,35 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404, redirec
 from django.contrib.auth.models import User
 from django.http import HttpResponse, Http404
 from django.contrib.auth.forms import UserCreationForm
-from .forms import ImageForm
 from django.contrib.auth import login
-
-from .models import Image, Tag, User, Comment
+from math import ceil
+from .models import Image, Tag, Comment
 from .handletags import strip_tagfield
+from .forms import ImageForm
 
 
-#Home view. Display user uploaded images by date. TODO: by views
-#Display 20 images at a time by descending order.
-#PARAM page = current page of images. (newest images at page no 1 etc)
+# Home view. Display user uploaded images by date. TODO: by views
+# Display 20 images at a time by descending order.
+# PARAM page = current page of images. (newest images at page no 1 etc)
 def upploudhome(request, cur_page=1):
-    #TODO: test for negative cur_page, implement fix
+    # TODO: test for negative cur_page, implement fix
     p = (cur_page - 1) * 20
     pn = p + 20
-    #Get 20 images in descending order
+    # calculate the number of image pages.
+    max_pages = ceil(Image.objects.all().count() / 20)
+    # set variable for next and previous pages. Required for url creation in template
+    next_page = cur_page + 1
+    prev_page = cur_page - 1
+    # Get 20 images in descending order
     images = Image.objects.order_by('-date')[p:pn]
     if not images:
         raise Http404()
 
-    return render( request, 'upploudhome.html', { 'images': images, 'cur_page': cur_page} )
+    return render(request, 'upploudhome.html', { 'images': images, 'cur_page': cur_page, 'next_page': next_page, 'prev_page': prev_page, 'max_pages': max_pages})
+
 
 def usersignup(request):
-    #redirect to user images instead if logged in
+    # redirect to user images instead if logged in
     if request.user.is_authenticated:
         return redirect('/upploud/userimages')
 
@@ -39,13 +45,15 @@ def usersignup(request):
 
     return render(request, "signup.html", {'sform': sform})
 
-#display user's uploaded images
+
+# display user's uploaded images
 def userimages(request):
     return render(request, "userimages.html")
 
-#upload view
+
+# upload view
 def upload(request):
-    #Redirect to login page if not logged in.
+    # Redirect to login page if not logged in.
     if not request.user.is_authenticated:
         return redirect('/upploud/login/')
 
@@ -56,7 +64,7 @@ def upload(request):
             image.uploaded_by = request.user
             image.save()
 
-            #adds tags to image.Tag manytomany field
+            # adds tags to image.Tag manytomany field
             strip_tagfield(request.POST.get('tagfield'), image)
 
             return redirect('/upploud/')
@@ -65,7 +73,8 @@ def upload(request):
 
     return render(request, "upload.html", {'form': form})
 
-#single image view
+
+# single image view
 def view(request, img_pk):
 
     img = get_object_or_404(Image, pk=img_pk)
